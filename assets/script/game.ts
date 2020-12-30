368fc6714a02feab0377978d20af6bc305065932
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-30 23:07:19 +0800

    切换修饰物

--- assets/script/game.ts
+++ assets/script/game.ts
@@ -9,13 +9,14 @@ export default class NewClass extends cc.Component
     @property({ type:[cc.SpriteFrame],displayName:"所有的背景资源"})
     bg_res: cc.SpriteFrame[] = Array<cc.SpriteFrame>();
 
-    @property({ type: cc.Button, displayName: "开始游戏" })
-    btnPlay: cc.Button = null;
+    @property({ type: [cc.Node], displayName: "所有的背景修饰资源" })
+    bg_decorator: cc.Node[] = Array<cc.Node>();
 
     onLoad()
     {
         cc.log("onLoad");
         this.bg_sprite.spriteFrame = this.bg_res[0];
+        this.bg_decorator[0].active = true;
     }
 
     start() 
@@ -32,9 +33,15 @@ export default class NewClass extends cc.Component
                 break;
             case "Setting":
                 cc.log("Setting!");
+                this.bg_sprite.spriteFrame = this.bg_res[0];
+                this.bg_decorator[0].active = true;
+                this.bg_decorator[1].active = false;
                 break;
             case "Skin":
                 cc.log("SKin!");
+                this.bg_sprite.spriteFrame = this.bg_res[1];
+                this.bg_decorator[0].active = false;
+                this.bg_decorator[1].active = true;
                 break;
             default:
                 cc.log("default");
