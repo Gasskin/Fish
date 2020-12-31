2c53be61bb6ea0d25fca080d3ee92ce15248a584
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-31 11:25:40 +0800

    随机生成水波

--- assets/script/game.ts
+++ assets/script/game.ts
@@ -12,9 +12,20 @@ export default class NewClass extends cc.Component
     @property({ type: [cc.Node], tooltip: "所有的背景修饰资源" })
     bg_decorator: cc.Node[] = Array<cc.Node>();
 
+    @property({ type: cc.Node, tooltip: "水波纹节点" })
+    ripple_node: cc.Node = null;
+
+    @property
+    cur_ripple_time: number = 0;
+
+    @property
+    random_ripple_time: number = Math.random() * 120 + 180;
+
     onLoad()
     {
         this.changeScene(5);
+        this.playRipple();
+        cc.log(this.random_ripple_time/60);
     }
 
     start() 
@@ -54,9 +65,6 @@ export default class NewClass extends cc.Component
      */
     changeScene(num: number)
     {
-        cc.log(num);
-        cc.log("res:" + (this.bg_res.length-1));
-        cc.log("decorator:" + (this.bg_decorator.length - 1));
         if (num > this.bg_res.length || num > this.bg_decorator.length)
         {
             cc.log("切换场景时数组越界");
@@ -88,6 +96,29 @@ export default class NewClass extends cc.Component
         }
     }
 
+    /**
+     * 在主页随机位置播放水波纹
+     */
+    playRipple()
+    {
+        let posx: number = Math.random() * 720 - 360;
+        let posy: number = Math.random() * 1280 - 640;
+        this.ripple_node.setPosition(posx, posy);
+        this.ripple_node.getComponent(cc.Animation).play("ripple");
+    }
 
-    // update (dt) {}
+    update(dt)
+    {
+        if (this.cur_ripple_time > this.random_ripple_time)
+        {
+            this.playRipple();
+            this.cur_ripple_time = 0;
+            this.random_ripple_time = Math.random() * 120 + 180;
+            cc.log(this.random_ripple_time/60);
+        }
+        else
+        {
+            this.cur_ripple_time++;
+        }
+    }
 }
