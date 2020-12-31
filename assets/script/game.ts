fc08cf044108d2cc57885278de19201b9c10879c
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-31 10:40:41 +0800

    场景设置

--- assets/script/game.ts
+++ assets/script/game.ts
@@ -3,50 +3,91 @@ const { ccclass, property } = cc._decorator;
 @ccclass
 export default class NewClass extends cc.Component 
 {
-    @property({ type:cc.Sprite,displayName:"当前背景Sprite" })
+    @property({ type:cc.Sprite,tooltip:"当前背景Sprite" })
     bg_sprite: cc.Sprite = null;
     
-    @property({ type:[cc.SpriteFrame],displayName:"所有的背景资源"})
+    @property({ type: [cc.SpriteFrame], tooltip:"所有的背景资源"})
     bg_res: cc.SpriteFrame[] = Array<cc.SpriteFrame>();
 
-    @property({ type: [cc.Node], displayName: "所有的背景修饰资源" })
+    @property({ type: [cc.Node], tooltip: "所有的背景修饰资源" })
     bg_decorator: cc.Node[] = Array<cc.Node>();
 
     onLoad()
     {
-        cc.log("onLoad");
-        this.bg_sprite.spriteFrame = this.bg_res[0];
-        this.bg_decorator[0].active = true;
+        this.changeScene(5);
     }
 
     start() 
     {
-        cc.log("start");
     }
-
+    
+    /**
+     * 游戏主界面所注册的按钮注册
+     * @param event 默认的消息集合
+     * @param type 传递的参数，用以区别不同的点击事件
+     */
     btnAction(event:Event,type:String)
     {
         switch (type)
         {
             case "Play":
                 cc.log("play!");
+                this.changeScene(7);
                 break;
             case "Setting":
                 cc.log("Setting!");
-                this.bg_sprite.spriteFrame = this.bg_res[0];
-                this.bg_decorator[0].active = true;
-                this.bg_decorator[1].active = false;
+                this.changeScene(8);
                 break;
             case "Skin":
                 cc.log("SKin!");
-                this.bg_sprite.spriteFrame = this.bg_res[1];
-                this.bg_decorator[0].active = false;
-                this.bg_decorator[1].active = true;
+                this.changeScene(9);
                 break;
             default:
                 cc.log("default");
                 break;
         }
     }
+
+    /**
+     * 用于切换不同风格的场景元素
+     * @param num 切换场景的编号，本游戏中共10个场景，即编号[0,9]
+     */
+    changeScene(num: number)
+    {
+        cc.log(num);
+        cc.log("res:" + (this.bg_res.length-1));
+        cc.log("decorator:" + (this.bg_decorator.length - 1));
+        if (num > this.bg_res.length || num > this.bg_decorator.length)
+        {
+            cc.log("切换场景时数组越界");
+            return;
+        }
+        else
+        {
+            //设置背景
+            for (var i = 0; i < this.bg_res.length; i++)
+            {
+                if (i == num)
+                {
+                    this.bg_sprite.spriteFrame = this.bg_res[i];
+                    break;
+                }
+            }
+            //设置风格元素
+            for (var i = 0; i < this.bg_decorator.length; i++)
+            {
+                if (i == num)
+                {
+                    this.bg_decorator[i].active = true;
+                }
+                else
+                {
+                    this.bg_decorator[i].active = false;
+                }
+            }
+        }
+    }
+
+
     // update (dt) {}
 }
