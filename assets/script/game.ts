541f0d40148588ab355cc749d439899aa64153b2
旧亚楠\Gascoigne <415753928@qq.com>
2020-12-31 17:45:47 +0800

    随机生成小鱼+转向

--- assets/script/game.ts
+++ assets/script/game.ts
@@ -4,28 +4,38 @@ const { ccclass, property } = cc._decorator;
 export default class NewClass extends cc.Component 
 {
     @property({ type:cc.Sprite,tooltip:"当前背景Sprite" })
-    bg_sprite: cc.Sprite = null;
-    
+    bg_sprite: cc.Sprite = null;   
     @property({ type: [cc.SpriteFrame], tooltip:"所有的背景资源"})
     bg_res: cc.SpriteFrame[] = Array<cc.SpriteFrame>();
-
     @property({ type: [cc.Node], tooltip: "所有的背景修饰资源" })
     bg_decorator: cc.Node[] = Array<cc.Node>();
-
     @property({ type: cc.Node, tooltip: "水波纹节点" })
     ripple_node: cc.Node = null;
+    @property({ type: [cc.Node], tooltip: "三种小鱼资源" })
+    fish_res: cc.Node[] = Array<cc.Node>();
+    @property({ type: [cc.Node], tooltip: "起始与结束位置" })
+    pos_start: cc.Node[] = Array<cc.Node>();
+    @property({ type: [cc.Node], tooltip: "途径位置1" })
+    pos_move: cc.Node[] = Array<cc.Node>();
+    @property({ type: [cc.Node], tooltip: "途径位置2" })
+    pos_end: cc.Node[] = Array<cc.Node>();
 
-    @property
+    @property({visible:false})
     cur_ripple_time: number = 0;
-
-    @property
+    @property({ visible: false })
+    cur_fish_time: number = 0;
+    @property({ visible: false })
     random_ripple_time: number = Math.random() * 120 + 180;
+    @property({ visible: false })
+    fish_type: number = null;
+    @property({ visible: false })
+    pre_pos: cc.Vec2 = null;
 
     onLoad()
     {
         this.changeScene(5);
         this.playRipple();
-        cc.log(this.random_ripple_time/60);
+        this.playFish();
     }
 
     start() 
@@ -107,18 +117,97 @@ export default class NewClass extends cc.Component
         this.ripple_node.getComponent(cc.Animation).play("ripple");
     }
 
+    /**
+     * 随机生成某种颜色的小鱼进行一次移动
+     */
+    playFish()
+    {
+        //随机获取一条鱼
+        this.fish_type = Math.floor(Math.random() * 3);
+        for (let i: number = 0; i < 3; i++)
+        {
+            if (i == this.fish_type)
+            {
+                this.fish_res[i].active = true;
+            }
+            else
+            {
+                this.fish_res[i].active = false;
+            }
+        }
+        //随机获取3个点，分别作为起始点，经过点，结束点
+        let start: number = Math.floor(Math.random() * 4);
+        let startPos: cc.Vec2 = cc.v2(this.pos_start[start].position.x, this.pos_start[start].position.y);
+
+        let start2: number = Math.floor(Math.random() * 4);
+        let startPos2: cc.Vec2 = cc.v2(this.pos_start[start2].position.x, this.pos_start[start2].position.y);
+
+        let move: number = Math.floor(Math.random() * 3);
+        let movePos: cc.Vec2 = cc.v2(this.pos_move[move].position.x, this.pos_move[move].position.y);
+
+        let end: number = Math.floor(Math.random() * 3);
+        let endPos: cc.Vec2 = cc.v2(this.pos_end[end].position.x, this.pos_end[end].position.y);
+
+        this.fish_res[this.fish_type].setPosition(startPos);
+        this.pre_pos = startPos;
+
+        let arr: cc.Vec2[] = [startPos, movePos, endPos, startPos2];
+        this.fish_res[this.fish_type].runAction(cc.cardinalSplineTo(8, arr, 0));
+    }
+
+    /**
+     * 给定两个点，返回他们之间的角度
+     * @param pos1 第一个点二维坐标
+     * @param pos2 第二个点的二维坐标
+     */
+    getAngle(pos1: cc.Vec2, pos2: cc.Vec2): number
+    {
+        var x = pos2.x - pos1.x;
+        var y = pos2.y - pos1.y;
+        var hypotenuse = Math.sqrt(x * x + y * y);
+        var cos = x / hypotenuse;
+        var radian = Math.acos(cos);
+        //弧度
+        var angle = 180 / (Math.PI / radian);
+        //角度
+        if (y < 0)
+        {
+            angle = 0 - angle;
+        }
+        else if (y == 0 && x < 0)
+        {
+            angle = 180;
+        }
+        return 90 - angle;
+    }
+
     update(dt)
     {
+        //随机生成水波
         if (this.cur_ripple_time > this.random_ripple_time)
         {
             this.playRipple();
             this.cur_ripple_time = 0;
             this.random_ripple_time = Math.random() * 120 + 180;
-            cc.log(this.random_ripple_time/60);
         }
         else
         {
             this.cur_ripple_time++;
+        }
+        //小鱼转动角度
+        let cur_pos: cc.Vec2 = cc.v2(this.fish_res[this.fish_type].position.x, this.fish_res[this.fish_type].position.y);
+        var angle = this.getAngle(this.pre_pos, cur_pos);
+        this.fish_res[this.fish_type].angle = -angle;
+        this.pre_pos = cur_pos;
+        //随机生成小鱼
+        if (this.cur_fish_time > 8)
+        {
+            this.cur_fish_time = 0;
+            this.playFish();
+        }
+        else
+        {
+            this.cur_fish_time += dt;
         }
     }
 }
