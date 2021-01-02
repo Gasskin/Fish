f7111411cdc0ed125ca9833415f6505066ab33fe
旧亚楠\Gascoigne <415753928@qq.com>
2021-01-02 11:57:32 +0800

    移动木块

--- assets/script/block.ts
+++ assets/script/block.ts
@@ -1,27 +1,43 @@
-// Learn TypeScript:
-//  - https://docs.cocos.com/creator/manual/en/scripting/typescript.html
-// Learn Attribute:
-//  - https://docs.cocos.com/creator/manual/en/scripting/reference/attributes.html
-// Learn life-cycle callbacks:
-//  - https://docs.cocos.com/creator/manual/en/scripting/life-cycle-callbacks.html
-
 const {ccclass, property} = cc._decorator;
 
 @ccclass
 export default class NewClass extends cc.Component {
 
-    @property(cc.Label)
-    label: cc.Label = null;
 
-    @property
-    text: string = 'hello';
+    //内部属性
+    default_pos: cc.Vec2 = null;
 
-    // LIFE-CYCLE CALLBACKS:
+    onLoad()
+    {
+        this.bindTouchFunc();
+    }
 
-    // onLoad () {}
+    start() 
+    {
 
-    start () {
+    }
 
+    /**
+     * 注册木块的触摸事件
+     */
+    bindTouchFunc()
+    {
+        this.node.on(cc.Node.EventType.TOUCH_START, function (event)
+        {
+            cc.log("start");
+            this.default_pos = this.node.getPosition();
+        }, this);
+        this.node.on(cc.Node.EventType.TOUCH_MOVE, function (event)
+        {
+            cc.log("move");
+            let move_dis: cc.Vec2 = event.getDelta();
+            this.default_pos = cc.v2(this.default_pos.x + move_dis.x, this.default_pos.y + move_dis.y);
+            this.node.setPosition(this.default_pos);
+        }, this);
+        this.node.on(cc.Node.EventType.TOUCH_END, function (event)
+        {
+            cc.log("end");
+        }, this);
     }
 
     // update (dt) {}
